eadee2b0e2f93b5ddb65ce960a66a86d97a21f1a
neonion12 <nion5404@gmail.com>
2024-07-04 19:38:55 +0600

    Version 4 :: July 4,2024 :: Rapl club joining setup added , about us button added

--- src/main/java/com/ProductStore/ctl/WelcomePageCtl.java
+++ src/main/java/com/ProductStore/ctl/WelcomePageCtl.java
@@ -3,7 +3,9 @@ package com.ProductStore.ctl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WelcomePageCtl {
@@ -11,8 +13,8 @@ public class WelcomePageCtl {
     @Autowired
     private Environment env;
 
-    @GetMapping("/welcomepage")
-    public String welcomePage() {
-        return "welcome";
+    @GetMapping("/welcome_rapl")
+    public String welcome_rapl_Page() {
+        return "welcome_rapl";
     }
 }
